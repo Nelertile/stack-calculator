8b00b151921ec656e89439dd840fdbee609548cf
NotToxic <47497223+NotToxicDev@users.noreply.github.com>
2022-03-15 16:28:16 +0100

    i fixed it

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,35 @@
-import inquirer from 'inquirer';
-import chalk from 'chalk';
-
-console.log(chalk.yellowBright("Minecraft Stack Calculator"))
-prompts()
-
-function prompts(){
-    inquirer.prompt([{
-        name: 'ItemAmount',
-        type: 'number',
-        message: 'Please enter your number of items.'
-    },{
-        name: 'action',
-        type: 'checkbox',
-        message: 'Select Actions',
-        choices: [{name: 'Round up', checked: true}, {name: 'Display only relevant variables'},{name: 'Calculate Shulkers'}],
-    }]).then((answers) =>{
-        actions(answers)
-    })
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+console.log("Minecraft Stack Calculator");
+prompts();
+function prompts() {
+    inquirer_1.default.prompt([{
+            name: 'ItemAmount',
+            type: 'number',
+            message: 'Please enter your number of items.'
+        }, {
+            name: 'action',
+            type: 'checkbox',
+            message: 'Select Actions',
+            choices: [{ name: 'Display only relevant variables' }, { name: 'Calculate Shulkers' }],
+        }]).then((answers) => {
+        actions(answers);
+    });
 }
-
 function actions(answers) {
-    let round = false;
     let compact = false;
     let calcshulker = false;
     for (let i = 0; i < answers.action.length; i++) {
-        if(answers.action[i] == 'Round up') round = true;
-        if(answers.action[i] == 'Display only relevant variables') compact = true;
-        if(answers.action[i] == 'Calculate Shulkers') calcshulker = true;
+        if (answers.action[i] == 'Display only relevant variables')
+            compact = true;
+        if (answers.action[i] == 'Calculate Shulkers')
+            calcshulker = true;
     }
-
-    let stack = answers.ItemAmount / 64
-    let stackout = stack.toFixed(0)
-    if(round && stack.toFixed(0) < stack) stackout = stack.toFixed(0) + 1
-    console.log(stackout)
+    let stack = answers.ItemAmount / 64;
+    let stackout = Math.ceil(stack);
+    console.log(stackout);
 }
--- src/index.ts
+++ src/index.ts
@@ -12,23 +12,21 @@ function prompts(){
         name: 'action',
         type: 'checkbox',
         message: 'Select Actions',
-        choices: [{name: 'Round up', checked: true}, {name: 'Display only relevant variables'},{name: 'Calculate Shulkers'}],
+        choices: [{name: 'Display only relevant variables'},{name: 'Calculate Shulkers'}],
     }]).then((answers) =>{
         actions(answers)
     })
 }
 
 function actions(answers: any) {
-    let round: boolean = false;
     let compact: boolean = false;
     let calcshulker: boolean = false;
     for (let i = 0; i < answers.action.length; i++) {
-        if(answers.action[i] == 'Round up') round = true;
         if(answers.action[i] == 'Display only relevant variables') compact = true;
         if(answers.action[i] == 'Calculate Shulkers') calcshulker = true;
     }
 
     let stack: number = answers.ItemAmount / 64
-    let stackout: number = stack
+    let stackout: number = Math.ceil(stack)
     console.log(stackout)
 }
